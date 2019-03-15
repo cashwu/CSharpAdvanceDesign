ff202e7859d24654854e6bdac823435324c9bc72
Cash Wu <cash@cashwu.com>
2019-03-15 14:34:09 +0800

    select - where & select

--- CSharpAdvanceDesignTests/JoeyWhereTests.cs
+++ CSharpAdvanceDesignTests/JoeyWhereTests.cs
@@ -1,6 +1,7 @@
 ﻿using Lab.Entities;
 using NUnit.Framework;
 using System.Collections.Generic;
+using System.Linq;
 using ExpectedObjects;
 using Lab;
 
@@ -31,6 +32,34 @@ namespace CSharpAdvanceDesignTests
                 new Product { Id = 2, Cost = 21, Price = 210, Supplier = "Yahoo" },
                 new Product { Id = 3, Cost = 31, Price = 310, Supplier = "Odd-e" },
                 new Product { Id = 4, Cost = 41, Price = 410, Supplier = "Odd-e" }
+            };
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        [Test]
+        public void where_select()
+        {
+            var products = new List<Product>
+            {
+                new Product { Id = 1, Cost = 11, Price = 110, Supplier = "Odd-e" },
+                new Product { Id = 2, Cost = 21, Price = 210, Supplier = "Yahoo" },
+                new Product { Id = 3, Cost = 31, Price = 310, Supplier = "Odd-e" },
+                new Product { Id = 4, Cost = 41, Price = 410, Supplier = "Odd-e" },
+                new Product { Id = 5, Cost = 51, Price = 510, Supplier = "Momo" },
+                new Product { Id = 6, Cost = 61, Price = 610, Supplier = "Momo" },
+                new Product { Id = 7, Cost = 71, Price = 710, Supplier = "Yahoo" },
+                new Product { Id = 8, Cost = 18, Price = 780, Supplier = "Yahoo" }
+            };
+
+            var actual = products.JoeyWhere(product => product.Price > 200 && product.Price < 500)
+                                 .JoeySelect(p => p.Price).ToList();
+
+            var expected = new[]
+            {
+                210,
+                310,
+                410
             };
 
             expected.ToExpectedObject().ShouldMatch(actual);
