8e673d3a62fc0cf11c6723ef4830fb4ad0a1c308
Cash Wu <cash@cashwu.com>
2019-03-15 15:05:58 +0800

    take - 2 employees

--- CSharpAdvanceDesignTests/JoeyTakeTests.cs
+++ CSharpAdvanceDesignTests/JoeyTakeTests.cs
@@ -7,40 +7,46 @@ using System.Collections.Generic;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture()]
-    [Ignore("not yet")]
     public class JoeyTakeTests
     {
         [Test]
         public void take_2_employees()
         {
-            var employees = GetEmployees();
+            var employees = (IEnumerable<Employee>)new List<Employee>
+            {
+                new Employee { FirstName = "Joey", LastName = "Chen" },
+                new Employee { FirstName = "Tom", LastName = "Li" },
+                new Employee { FirstName = "David", LastName = "Chen" },
+                new Employee { FirstName = "Mike", LastName = "Chang" },
+                new Employee { FirstName = "Joseph", LastName = "Yao" },
+            };
 
             var actual = JoeyTake(employees);
 
             var expected = new List<Employee>
             {
-                new Employee {FirstName = "Joey", LastName = "Chen"},
-                new Employee {FirstName = "Tom", LastName = "Li"},
+                new Employee { FirstName = "Joey", LastName = "Chen" },
+                new Employee { FirstName = "Tom", LastName = "Li" },
             };
 
-            expected.ToExpectedObject().ShouldEqual(actual);
+            expected.ToExpectedObject().ShouldMatch(actual);
         }
 
         private IEnumerable<Employee> JoeyTake(IEnumerable<Employee> employees)
         {
-            throw new System.NotImplementedException();
-        }
+            var enumerator = employees.GetEnumerator();
 
-        private static IEnumerable<Employee> GetEmployees()
-        {
-            return new List<Employee>
+            var index = 0;
+            while (enumerator.MoveNext())
             {
-                new Employee {FirstName = "Joey", LastName = "Chen"},
-                new Employee {FirstName = "Tom", LastName = "Li"},
-                new Employee {FirstName = "David", LastName = "Chen"},
-                new Employee {FirstName = "Mike", LastName = "Chang"},
-                new Employee {FirstName = "Joseph", LastName = "Yao"},
-            };
+                yield return enumerator.Current;
+                index++;
+                
+                if (index == 2)
+                {
+                    yield break;
+                }
+            }
         }
     }
 }
