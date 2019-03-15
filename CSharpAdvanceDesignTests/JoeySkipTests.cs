1bcc68f227748b56db36c4ebf5129aad1374ae17
Cash Wu <cash@cashwu.com>
2019-03-15 15:17:19 +0800

    skip - 2 employees

--- CSharpAdvanceDesignTests/JoeySkipTests.cs
+++ CSharpAdvanceDesignTests/JoeySkipTests.cs
@@ -7,41 +7,45 @@ using System.Linq;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeySkipTests
     {
         [Test]
         public void skip_2_employees()
         {
-            var employees = GetEmployees();
+            var employees = (IEnumerable<Employee>)new List<Employee>
+            {
+                new Employee { FirstName = "Joey", LastName = "Chen" },
+                new Employee { FirstName = "Tom", LastName = "Li" },
+                new Employee { FirstName = "David", LastName = "Chen" },
+                new Employee { FirstName = "Mike", LastName = "Chang" },
+                new Employee { FirstName = "Joseph", LastName = "Yao" },
+            };
 
-            var actual = JoeySelect(employees);
+            var actual = JoeySkip(employees);
 
             var expected = new List<Employee>
             {
-                new Employee {FirstName = "David", LastName = "Chen"},
-                new Employee {FirstName = "Mike", LastName = "Chang"},
-                new Employee {FirstName = "Joseph", LastName = "Yao"},
+                new Employee { FirstName = "David", LastName = "Chen" },
+                new Employee { FirstName = "Mike", LastName = "Chang" },
+                new Employee { FirstName = "Joseph", LastName = "Yao" },
             };
 
-            expected.ToExpectedObject().ShouldEqual(actual.ToList());
+            expected.ToExpectedObject().ShouldMatch(actual.ToList());
         }
 
-        private IEnumerable<Employee> JoeySelect(IEnumerable<Employee> employees)
+        private IEnumerable<Employee> JoeySkip(IEnumerable<Employee> employees)
         {
-            throw new System.NotImplementedException();
-        }
+            var enumerator = employees.GetEnumerator();
 
-        private static IEnumerable<Employee> GetEmployees()
-        {
-            return new List<Employee>
+            var index = 0;
+            while (enumerator.MoveNext())
             {
-                new Employee {FirstName = "Joey", LastName = "Chen"},
-                new Employee {FirstName = "Tom", LastName = "Li"},
-                new Employee {FirstName = "David", LastName = "Chen"},
-                new Employee {FirstName = "Mike", LastName = "Chang"},
-                new Employee {FirstName = "Joseph", LastName = "Yao"},
-            };
+                if (index >= 2)
+                {
+                    yield return enumerator.Current;
+                }
+                index++;
+            }
         }
     }
 }
