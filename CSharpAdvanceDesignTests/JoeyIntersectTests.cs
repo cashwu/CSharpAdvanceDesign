8ffb4fe6a4c77337cc4757a307703a19a4bdc8f3
Cash Wu <cash@cashwu.com>
2019-03-17 10:44:41 +0800

    intersect

--- CSharpAdvanceDesignTests/JoeyIntersectTests.cs
+++ CSharpAdvanceDesignTests/JoeyIntersectTests.cs
@@ -5,13 +5,12 @@ using System.Collections.Generic;
 namespace CSharpAdvanceDesignTests
 {
     [TestFixture]
-    [Ignore("not yet")]
     public class JoeyIntersectTests
     {
         [Test]
         public void intersect_numbers()
         {
-            var first = new[] { 1, 3, 5 };
+            var first = new[] { 1, 3, 5, 3 };
             var second = new[] { 5, 7, 3 };
 
             var actual = JoeyIntersect(first, second);
@@ -23,7 +22,18 @@ namespace CSharpAdvanceDesignTests
 
         private IEnumerable<int> JoeyIntersect(IEnumerable<int> first, IEnumerable<int> second)
         {
-            throw new System.NotImplementedException();
+            var hashSet = new HashSet<int>(second);
+            
+            var firstEnumerator = first.GetEnumerator();
+            while (firstEnumerator.MoveNext())
+            {
+                var current = firstEnumerator.Current;
+
+                if (hashSet.Remove(current))
+                {
+                    yield return current;
+                }
+            }
         }
     }
 }
