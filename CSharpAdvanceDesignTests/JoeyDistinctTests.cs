65404b91495757f9d7462c560bbc2a4b390be488
Cash Wu <cash@cashwu.com>
2019-03-17 09:48:45 +0800

    distinct - EqualityComparer

--- CSharpAdvanceDesignTests/JoeyDistinctTests.cs
+++ CSharpAdvanceDesignTests/JoeyDistinctTests.cs
@@ -1,7 +1,7 @@
 ﻿using ExpectedObjects;
 using NUnit.Framework;
-using NUnit.Framework.Internal;
 using System.Collections.Generic;
+using Lab.Entities;
 
 namespace CSharpAdvanceDesignTests
 {
@@ -12,7 +12,7 @@ namespace CSharpAdvanceDesignTests
         public void distinct_numbers()
         {
             var numbers = new[] { 91, 3, 91, -1 };
-            var actual = Distinct(numbers);
+            var actual = JoeyDistinct(numbers);
 
             var expected = new[] { 91, 3, -1 };
 
@@ -23,20 +23,41 @@ namespace CSharpAdvanceDesignTests
         public void distinct_numbers_2()
         {
             var numbers = new[] { 91, 3, -1, 91, 3 };
-            var actual = Distinct(numbers);
+            var actual = JoeyDistinct(numbers);
 
             var expected = new[] { 91, 3, -1 };
 
             expected.ToExpectedObject().ShouldMatch(actual);
         }
 
-        private IEnumerable<int> Distinct(IEnumerable<int> numbers)
+        [Test]
+        public void distinct_employees()
         {
-            // return new HashSet<int>(numbers);
+            var employees = new[]
+            {
+                new Employee { FirstName = "Joey", LastName = "Chen" },
+                new Employee { FirstName = "Joseph", LastName = "Chen" },
+                new Employee { FirstName = "Tom", LastName = "Li" },
+                new Employee { FirstName = "Joey", LastName = "Chen" },
+            };
 
-            var hashSet = new HashSet<int>();
+            var expected = new[]
+            {
+                new Employee { FirstName = "Joey", LastName = "Chen" },
+                new Employee { FirstName = "Joseph", LastName = "Chen" },
+                new Employee { FirstName = "Tom", LastName = "Li" },
+            };
 
-            var enumerator = numbers.GetEnumerator();
+            var actual = JoeyDistinct(employees, new JoeyEmployeeEqualityComparer());
+
+            expected.ToExpectedObject().ShouldMatch(actual);
+        }
+
+        private IEnumerable<TSource> JoeyDistinct<TSource>(IEnumerable<TSource> sources, IEqualityComparer<TSource> comparer)
+        {
+            var hashSet = new HashSet<TSource>(comparer);
+
+            var enumerator = sources.GetEnumerator();
             while (enumerator.MoveNext())
             {
                 var current = enumerator.Current;
@@ -46,6 +67,11 @@ namespace CSharpAdvanceDesignTests
                     yield return current;
                 }
             }
+        }
+
+        private IEnumerable<TSource> JoeyDistinct<TSource>(IEnumerable<TSource> sources)
+        {
+            return JoeyDistinct(sources, EqualityComparer<TSource>.Default);
         }
     }
 }
