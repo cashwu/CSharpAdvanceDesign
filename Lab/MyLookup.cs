f72e78c6da04509608db7bb9a0ce742577b9a22e
Cash <cash@cashwu.com>
2019-08-29 11:46:21 +0800

    commit

---
+++ Lab/MyLookup.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Lab.Entities;
+
+namespace Lab
+{
+    internal class MyLookup : IEnumerable<IGrouping<string, Employee>>
+    {
+        private readonly Dictionary<string, List<Employee>> _myLookup = new Dictionary<string, List<Employee>>();
+
+        public IEnumerator<IGrouping<string, Employee>> GetEnumerator()
+        {
+            return _myLookup.Select(a => new MyGrouping(a.Key, a.Value)).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void AddElement(string key, Employee value)
+        {
+            if (_myLookup.ContainsKey(key))
+            {
+                _myLookup[key].Add(value);
+            }
+            else
+            {
+                _myLookup.Add(key, new List<Employee> { value });
+            }
+        }
+    }
+}
